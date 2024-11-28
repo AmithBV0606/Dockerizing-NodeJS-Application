df086ea2cba24926592a276fb3818b1dd2af53cc
AmithBV0606 <amithrao0606@gmail.com>
2024-11-28 13:40:07 +0530

    Optimized the Dockerfile

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,14 @@ app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript");
 });
 
+app.get("/about", (req: Request, res: Response) => {
+  res.send("My name is Amith!!");
+});
+
+app.get("/contact", (req: Request, res: Response) => {
+  res.send("Ph Number : +91 6361670056");
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
